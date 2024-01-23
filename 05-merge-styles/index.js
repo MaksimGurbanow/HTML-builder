0c4151fe8cbc1a7c47885452b9ca9603de201b04
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-23 18:19:03 +0300

    fix: fix problems in 5 tasks

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,10 +1,13 @@
-const { readdir, readFile, writeFile } = require('fs/promises');
+const { readdir, readFile, writeFile, mkdir } = require('fs/promises');
 const path = require('path');
 
 const distFolderPath = path.join(__dirname, 'project-dist');
 
 // I choosed recursive because i didn't understand whether i have to bundle all styles folders in project
-readdir(__dirname, { withFileTypes: true, recursive: true })
+readdir(path.join(__dirname, 'styles'), {
+  withFileTypes: true,
+  recursive: true,
+})
   .then((files) => {
     const filteredFiles = files.filter(
       (file) =>
@@ -20,5 +23,6 @@ readdir(__dirname, { withFileTypes: true, recursive: true })
   })
   .then((fileContents) => {
     const accum = fileContents.join('\n');
+    mkdir(distFolderPath, { recursive: true });
     return writeFile(path.join(distFolderPath, 'bundle.css'), accum);
   });
