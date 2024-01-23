ac1efc1dffed1037bc344c711c25e284804acd5c
Maksim Gurbanow <maksim20051708@gmail.com>
2024-01-23 17:38:08 +0300

    fix: fix problems in 4 tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,16 +1,24 @@
-const { copyFile, mkdir, readdir } = require('fs/promises');
+const { copyFile, mkdir, readdir, rm, access } = require('fs/promises');
 const path = require('path');
 
-mkdir(path.join(__dirname, 'files-copy'), { recursive: true }).then(() => {
-  readdir(path.join(__dirname, 'files'), {
-    withFileTypes: true,
-    recursive: true,
-  }).then((files) => {
-    files.forEach((file) => {
-      copyFile(
-        path.join(__dirname, 'files', file.name),
-        path.join(__dirname, 'files-copy', file.name),
-      );
-    });
-  });
-});
+const copyPath = path.join(__dirname, 'files-copy');
+
+access(copyPath)
+  .catch(() => mkdir(copyPath))
+  .then(() =>
+    rm(copyPath, { recursive: true }, (err) => console.log(err)).then(() => {
+      mkdir(copyPath, { recursive: true }).then(() => {
+        readdir(path.join(__dirname, 'files'), {
+          withFileTypes: true,
+          recursive: true,
+        }).then((files) => {
+          files.forEach((file) => {
+            copyFile(
+              path.join(__dirname, 'files', file.name),
+              path.join(copyPath, file.name),
+            );
+          });
+        });
+      });
+    }),
+  );
